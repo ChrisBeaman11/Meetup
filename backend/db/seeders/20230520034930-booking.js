b2ec13cbd31bd37977530d54786f580a1e26c31a
Christopher Beaman <ChrisBeaman97@gmail.com>
2023-09-21 03:40:24 -0400

    more seeders

--- backend/db/seeders/20230520034930-booking.js
+++ backend/db/seeders/20230520034930-booking.js
@@ -17,13 +17,19 @@ module.exports = {
       },
       {
         spotId: 2,
-        userId: 2,
+        userId: 1,
         startDate: "2021-10-19",
         endDate: "2021-10-20",
       },
       {
         spotId: 3,
-        userId: 3,
+        userId: 1,
+        startDate: "2021-12-19",
+        endDate: "2021-12-20",
+      },
+      {
+        spotId: 4,
+        userId: 1,
         startDate: "2021-12-19",
         endDate: "2021-12-20",
       }
